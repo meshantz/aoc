f99dead36a7abeb6ce73411c9526c88e02ca11a1
Mike Shantz <mike.shantz.08@gmail.com>
2023-12-01 19:25:23 -0500

    overengineer the parsing

--- aoc2023/day01.py
+++ aoc2023/day01.py
@@ -1,4 +1,7 @@
 from utils import load
+import typing as t
+from dataclasses import dataclass
+
 
 digits_as_words = {
     "one": 1,
@@ -13,8 +16,34 @@ digits_as_words = {
 }
 
 
-def parse(raw_data):
-    return raw_data.splitlines()
+class LineConsumer(t.Protocol):
+    @classmethod
+    def from_lines(cls: t.Type[t.Self], x: t.Iterator[str]) -> t.Self:
+        ...
+
+
+T = t.TypeVar("T", bound=LineConsumer)
+
+
+@dataclass
+class WholeLine(LineConsumer):
+    data: str
+
+    @classmethod
+    def from_lines(cls: t.Type[t.Self], data_iter: t.Iterator[str]) -> t.Self:
+        return cls(next(data_iter))
+
+
+def parse(raw_data: str, factory: t.Type[T]) -> list[T]:
+    lines_iter = iter(raw_data.splitlines())
+    parsed = []
+    while True:
+        try:
+            parsed.append(factory.from_lines(lines_iter))
+        except StopIteration:
+            break
+
+    return parsed
 
 
 def make_digits(row: str, index: int) -> int:
@@ -28,19 +57,19 @@ def make_digits(row: str, index: int) -> int:
     return 0
 
 
-def part1(data):
+def part1(data: list[WholeLine]):
     sum = 0
     for row in data:
-        numbers = [c for c in row if c.isdigit()]
+        numbers = [c for c in row.data if c.isdigit()]
         sum += int(f"{numbers[0]}{numbers[-1]}")
 
     return sum
 
 
-def part2(data):
+def part2(data: list[WholeLine]):
     sum = 0
     for row in data:
-        numbers = [processed for i in range(len(row)) if (processed := make_digits(row, i))]
+        numbers = [processed for i in range(len(row.data)) if (processed := make_digits(row.data, i))]
         sum += int(f"{numbers[0]}{numbers[-1]}")
 
     return sum
@@ -48,7 +77,7 @@ def part2(data):
 
 def solution():
     raw_data = load("data/2023/day01.txt")
-    formatted_data = parse(raw_data)
+    formatted_data = parse(raw_data, WholeLine)
 
     answer1 = part1(formatted_data)
     print(f"Part 1: {answer1}")
@@ -62,7 +91,7 @@ def test():
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet"""
-    answer1 = part1(parse(test1))
+    answer1 = part1(parse(test1, WholeLine))
     print(f"Part 1: {answer1}")
     assert answer1 == 142
 
@@ -73,6 +102,6 @@ xtwone3four
 4nineeightseven2
 zoneight234
 7pqrstsixteen"""
-    answer2 = part2(parse(test2))
+    answer2 = part2(parse(test2, WholeLine))
     print(f"Part 2: {answer2}")
     assert answer2 == 281
