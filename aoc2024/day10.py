e456312bffcf3f77a056a82443434157d8d2707d
Mike Shantz <mike.shantz.08@gmail.com>
2024-12-11 20:17:23 -0500

    day 10 solution parts 1 and 2

--- aoc2024/day10.py
+++ aoc2024/day10.py
@@ -3,6 +3,7 @@ import contextlib
 import typing as t
 from dataclasses import dataclass
 from dataclasses import field
+import heapq
 
 import common
 
@@ -53,6 +54,9 @@ class Coordinate:
     def __mul__(self, other: int):
         return Coordinate(self.x * other, self.y * other)
 
+    def __lt__(self, other):
+        return (self.x, self.y) < (other.x, other.y)
+
 
 @dataclass
 class Grid:
@@ -65,6 +69,11 @@ class Grid:
         for _ in range(size.y):
             positions.append([None for __ in range(size.x)])
         return cls(size, positions)
+
+    def at(self, pos: Coordinate):
+        if pos.x >= self.size.x or pos.x < 0 or pos.y >= self.size.y or pos.y < 0:
+            return None
+        return self.values[pos.y][pos.x]
 
 
 def cardinals():
@@ -84,13 +93,57 @@ def neighbor_coords(pos: Coordinate, include_self: bool = False):
         yield pos
 
 
+def score_trail(start: Coordinate, grid: Grid):
+    nines: t.Set[Coordinate] = set()
+    visited: dict[Coordinate, int] = {}
+    edges: list[tuple[int, int, Coordinate]] = []
+    heapq.heappush(edges, (0, 0, start))
+    # bail = 4
+    while edges:
+        # print(edges)
+        distance, height, pos = heapq.heappop(edges)
+        edge_val = grid.at(pos)
+        # print("considering", pos, distance, height, "=", edge_val)
+        if edge_val == height:
+            # print("visiting", pos)
+            visited[pos] = max(distance, visited.get(pos, -1))
+            if edge_val == 9:
+                # print("END OF TRAIL!")
+                nines.add(pos)
+            else:
+                for neighbor in neighbor_coords(pos):
+                    if visited.get(neighbor, -1) < distance + 1:
+                        # print("adding", distance + 1, height + 1, neighbor)
+                        heapq.heappush(edges, (distance + 1, height + 1, neighbor))
+        # if not bail:
+        #     break
+        # bail -= 1
+    return len(nines)
+
+
+def collect(pos: Coordinate, grid: Grid, so_far: t.Set[Coordinate]) -> t.Set[t.FrozenSet[Coordinate]]:
+    pass_on: t.Set[Coordinate] = {pos} | so_far
+    height = grid.at(pos)
+
+    if height is None:
+        return set()
+    if height == 9:
+        return {frozenset(pass_on)}
+
+    result = set()
+    for n in neighbor_coords(pos):
+        if grid.at(n) == height + 1:
+            result |= collect(n, grid, pass_on)
+
+    return result
+
+
 def part1(data: Topography):
-    print(data)
-    return 0
+    return sum(score_trail(th, data.grid) for th in data.trail_heads)
 
 
 def part2(data: Topography):
-    return 0
+    return sum(len(collect(th, data.grid, set())) for th in data.trail_heads)
 
 
 def solution():
@@ -142,13 +195,37 @@ def test():
 32019012
 01329801
 10456732
-"""  # has 5, 6, 5, 3, 1, 3, 5, 3, and 5 = 36
+"""  # has 5, 6, 5, 3, 1, 3, 5, 3, and 5 = 36 and score 81
 
     use_data = test5
     answer1 = part1(common.parse_all(use_data, Topography))
     print(f"Part 1: {answer1}")
     # assert answer1 == ???
 
+    test6 = """.....0.
+..4321.
+..5..2.
+..6543.
+..7..4.
+..8765.
+..9....
+"""  # score 3
+    test7 = """..90..9
+...1.98
+...2..7
+6543456
+765.987
+876....
+987....
+"""
+    test8 = """012345
+123456
+234567
+345678
+4.6789
+56789.
+"""
+    use_data = test5
     answer2 = part2(common.parse_all(use_data, Topography))
     print(f"Part 2: {answer2}")
     # assert answer2 == ???
